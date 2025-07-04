a21ee2434857dc25934f7620938f354551f2a0a5
poojithanadendla <Poojithanadendla01@gmail.com>
2025-07-04 21:12:07 +0000

    Test CI/CD update1

--- app/app.py
+++ app/app.py
@@ -1,1 +1,6 @@
-print("Hello from GitHub Actions CI/CD!")
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    return 'Hello from GitHub Actions CI/CD!'
+app.run(host='0.0.0.0', port=80)
