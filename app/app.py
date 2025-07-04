7ab3c8ff16a40b2a1a2c898c42c4ab95efc61476
poojithanadendla <Poojithanadendla01@gmail.com>
2025-07-04 21:01:34 +0000

    Test CI/CD update

--- app/app.py
+++ app/app.py
@@ -1,9 +1,1 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Hello from Dockerized Flask App on AWS EC2!"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+print("Hello from GitHub Actions CI/CD!")
